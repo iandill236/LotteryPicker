9965d07b1bdbfc044c1f156ba951a6d9f9aa801b
BuildTools <unconfigured@null.spigotmc.org>
2020-05-10 18:26:55 -0400

    Add project files.

---
+++ LotteryPicker/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace LotteryPicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.generateButton = new System.Windows.Forms.Button();
+            this.winLabel = new System.Windows.Forms.Label();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(355, 271);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 23);
+            this.generateButton.TabIndex = 0;
+            this.generateButton.Text = "GENERATE";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.Location = new System.Drawing.Point(352, 77);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(91, 13);
+            this.winLabel.TabIndex = 1;
+            this.winLabel.Text = "Winning Numbers";
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Location = new System.Drawing.Point(355, 111);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(0, 13);
+            this.numberLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.winLabel);
+            this.Controls.Add(this.generateButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button generateButton;
+        private System.Windows.Forms.Label winLabel;
+        private System.Windows.Forms.Label numberLabel;
+    }
+}
+
---
+++ LotteryPicker/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LotteryPicker
+{
+    public partial class Form1 : Form
+    {
+        Random randGen = new Random();
+
+        int numberOne;
+        int numberTwo;
+        int numberThree;
+        int numberFour;
+        int numberFive;
+        int numberSix;
+        
+
+        const int maxValue = 100;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            numberOne = randGen.Next(1, 100);
+            numberTwo = randGen.Next(1, 100);
+           while (numberTwo == numberOne)
+            {
+                Random randGen = new Random();
+
+                numberTwo = randGen.Next(1, 100);
+            }
+        numberThree = randGen.Next(1, 100);
+        while (numberThree == numberTwo)
+            {
+                Random randGen = new Random();
+
+                numberThree = randGen.Next(1, 100);
+            }
+            numberFour = randGen.Next(1, 100);
+            while(numberFour == numberThree)
+            {
+                Random randGen = new Random();
+
+                numberFour = randGen.Next(1, 100);
+            }
+            numberFive = randGen.Next(1, 100);
+            while(numberFive == numberFour)
+            {
+                Random randGen = new Random();
+
+                numberFive = randGen.Next(1, 100);
+            }
+            numberSix = randGen.Next(1, 100);
+            while(numberSix == numberFive)
+            {
+                Random randGen = new Random();
+
+                numberSix = randGen.Next(1, 100);
+            }
+
+            numberLabel.Text = $"{numberOne} {numberTwo} {numberThree} {numberFour} {numberFive} {numberSix}";
+
+            
+        }
+    }
+}
